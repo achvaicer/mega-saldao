9d79b5fac446071fc8dccbf65aec863b4e300a9b
Alberto Chvaicer <achvaicer@gmail.com>
2012-05-15 00:32:20 -0300

    classe de produto

---
+++ src/mega-saldao/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mega_saldao.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int QuantityAvailable { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
